40d87354aa874b8ea45a31fc054a994fb8180f9b
manikanta2901 <manikanta2901@gmail.com>
2021-07-26 20:33:12 +0530

    Longest common subsequence solution added

---
+++ DynamicProgramming/LongestCommonSubsequence/Recursion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int LongestCommonSubsequence(string a,string b,int m,int n){
+    // Using Recursion
+    if(m == 0 || n == 0){
+        return 0;
+    }
+    if(a[m - 1] == b[n - 1]){
+        return 1 + LongestCommonSubsequence(a,b,m - 1,n - 1);
+    }else{
+        return (max(LongestCommonSubsequence(a,b,m - 1,n),LongestCommonSubsequence(a,b,m,n-1)));
+    }
+}
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+    cout << "Longest Common Subsequence is : " << LongestCommonSubsequence(a,b,a.size(),b.size()) << endl;
+    return 0;
+}
---
+++ DynamicProgramming/LongestCommonSubsequence/TopDown.cpp
@@ -0,0 +1,39 @@
+// Time Complexity is O(m * n )
+// Top down approach
+// As recursion goes upto  calls from higher functions to base case
+// so we are going to the Top to bottom 
+// Memoization
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int LongestCommonSubsequence(string a,string b,int m,int n){
+
+    vector<vector<int> > mat(m + 1,vector<int>(n + 1, -1));
+    if(mat[m][n] != -1){
+        return 0;
+    }
+    if(m == 0 || n == 0){
+        mat[m][n] = 0;
+    }else{
+        if(a[m - 1] == b[n - 1]){
+            mat[m][n] = 1 + LongestCommonSubsequence(a,b,m - 1,n - 1);
+        }else{
+            mat[m][n] = (max(LongestCommonSubsequence(a,b,m - 1,n),LongestCommonSubsequence(a,b,m,n-1)));
+        }
+    }
+    return mat[m][n];
+}
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+    cout << "Longest Common Subsequence is : " << LongestCommonSubsequence(a,b,a.size(),b.size()) << endl;
+    // for(auto it : mat){
+    //     for(auto i : it){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+    return 0;
+}
